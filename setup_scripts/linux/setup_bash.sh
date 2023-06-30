9139f81a137aed86d39df19399b0beef26f8f475
catwang01 <edwardelricwzx@gmail.com>
2023-06-30 17:37:59 +0800

    update setup files

--- setup_scripts/linux/setup_bash.sh
+++ setup_scripts/linux/setup_bash.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-if [ -e ~/.bashrc ]; 
+if [ ! -L ~/.bashrc ]; 
 then
     if [ ! -d ~/.bash ]; then
         mkdir ~/.bash
     fi
     cat ~/.bashrc >> ~/.bash/my_bashrc_private
     mv ~/.bashrc ~/.bashrc.back
-else
-    echo 'bashrc is not a symbol link'
 fi
