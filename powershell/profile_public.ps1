f1f045b73f7aa7d1072fbc090586cec2c47598b0
catwang01 <edwardelricwzx@gmail.com>
2023-07-18 19:07:24 +0800

    use region in install.conf.win32.yaml and support zlocation

--- powershell/profile_public.ps1
+++ powershell/profile_public.ps1
@@ -92,3 +92,6 @@ Function gig {
   $params = ($list | ForEach-Object { [uri]::EscapeDataString($_) }) -join ","
   Invoke-WebRequest -Uri "https://www.toptal.com/developers/gitignore/api/$params" | select -ExpandProperty content | Out-File -FilePath $(Join-Path -path $pwd -ChildPath ".gitignore") -Encoding ascii
 }
+
+
+Import-Module ZLocation;
