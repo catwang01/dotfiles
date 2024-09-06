6d558a2ff79b770f822b0f44db05dfb9d4269f11
catwang01 <edwardelricwzx@gmail.com>
2024-09-06 13:24:18 +0800

    Add keyviz related setup scripts for win32

---
+++ setup_scripts/win32/setup_keyviz.ps1
@@ -0,0 +1,36 @@
+# TODO: extract the common code to a shared script
+# this logic can be used for softwares like altdrag, kanata, keyviz, listary, which should be autostarted:
+. "$PSScriptRoot\utils.ps1"
+
+Set-StrictMode -Version Latest
+
+$keyVizHome = "$HOME/AppData/Local/Programs/Keyviz"
+$exePath = "$keyVizHome\Keyviz.exe"
+
+if (Test-Path $exePath)
+{
+    Write-Host "Keyviz is already installed"
+}
+else
+{
+    Write-Host "Keyviz is not installed, start installing it..."
+    winget-install "keyviz"
+    Write-Host "Keyviz is installed successfully"
+}
+
+$scriptBlock = [scriptblock]::Create(@"
+    if (Get-Process | Where-Object { \$_.Name -eq 'Keyviz' })
+    {
+        Write-Host "Keyviz is already running"
+    }
+    else
+    {
+        & $exePath
+    }
+"@)
+
+&$scriptBlock
+
+Register-StartUp -scriptBlock $scriptBlock `
+        -scheduleJobName "dotfile-Keyviz" `
+        -workingDirectory $altDragHome
